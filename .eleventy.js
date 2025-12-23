e20ad48d25bd4ce4c342a330a22a2701bd5a6dfe
eld120 <seyamack@gmail.com>
2025-12-23 16:12:22 -0600

    feat: Dynamically generate copyright year in footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
     eleventyConfig.addPassthroughCopy("./src/static/css/styles.css");
     eleventyConfig.addPassthroughCopy("./src/static/js")
     eleventyConfig.addPassthroughCopy("./src/assets/");
